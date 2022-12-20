8db0daac25e49f09bb1d16d0b0bd7f26cfa03803
saurav4986 <saurav.raj4986@gmail.com>
2022-12-20 23:22:06 +0530

    Welcome to Cab-Invoice-Generator

---
+++ src/main/java/com/bridgeLabz/App.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz;
+
+/**
+ * Welcome to Cab-Invoice-Generator
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Cab-Invoice-Generator!" );
+    }
+}
