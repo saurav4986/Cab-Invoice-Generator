a9318a55d5d401ee61fd98a4df2a93c26895e505
saurav4986 <saurav.raj4986@gmail.com>
2022-12-21 17:56:04 +0530

    Added: codes to calculate total fare for multiple rides

--- src/main/java/com/bridgeLabz/InvoiceGenerator.java
+++ src/main/java/com/bridgeLabz/InvoiceGenerator.java
@@ -11,21 +11,25 @@ public class InvoiceGenerator
         System.out.println( "Welcome to Cab-Invoice-Generator!" );
 
         //Step 1: Calculating total fare which returns the total fare
-        double totalFare = totalFare(0, 4); // distance,time
-        double totalFare2 = totalFare(10, 4); // distance,time
+        double totalFare = calculateFare(0, 4); // distance,time
+        double totalFare2 = calculateFare(10, 4); // distance,time
         System.out.println(totalFare);
         System.out.println(totalFare2);
     }
 
-    public static double totalFare(double distance, int time) {
-        double totalCostForDist = distance * COST_PER_KM;
-        int totalCostForTime = time * COST_PER_MINUTE;
-        double totalFare = totalCostForDist + totalCostForTime;
-        // if the total cost is lower than the 5 INR then minimum fare would be 5 INR
-        if (totalFare < MINIMUM_FARE) {
-            totalFare = MINIMUM_FARE;
-        }
-        System.out.println("Total fare of the journey is: " + totalFare);
+    public static double calculateFare(double distance, int time){
+        double totalFare= distance *COST_PER_KM+time*COST_PER_MINUTE;
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
         return totalFare;
     }
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride:rides)
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        return totalFare;
+    }
+
+
 }
